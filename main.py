af09228432336ccb086ff49289f6b308b028b3b9
Johnny Clark <johnnyclark90@gmail.com>
2024-05-16 13:53:16 -0700

    fix(reminders): prevent duplicate rent reminders and respect rent paid status
    Consolidated rent reminder logic within the `sunset_reminder` function
    to ensure rent reminders are only sent from a single source. Added
    checks to verify the rent paid status before sending reminders.

--- main.py
+++ main.py
@@ -36,12 +36,15 @@ class CommandHandler:
                     if command["arg_num"] == 0:
                         if command["async"]:
                             return await command["function"](message, self.client, args)
+                        else:
+                            return await message.channel.send(
+                                str(command["function"](message, self.client, args))
+                            )
 
-
-logger = logging.getLogger("armadyne")
 
 load_dotenv()
 
+logger = logging.getLogger("armadyne")
 logger.setLevel(logging.INFO)
 
 intents = discord.Intents.all()
@@ -128,11 +131,8 @@ async def check_rent_status(ctx):
 
 async def sunset_reminder():
     await bot.wait_until_ready()
-    print("sunset_reminder function has started.")  # Debug point 1
     tz = pytz.timezone(location_timezone)
-
     current_date = datetime.now(tz).date()
-    print(f"Current date set to {current_date}.")  # Debug point 2
 
     while not bot.is_closed():
         now = datetime.now(tz)
@@ -142,7 +142,6 @@ async def sunset_reminder():
             db_handler.set_rent_paid(False)
             logger.info("Reset rent paid status for the new month.")
 
-        # If the function's internal date is behind the system date, correct it
         if today > current_date:
             current_date = today
 
@@ -150,37 +149,28 @@ async def sunset_reminder():
         sunset_time = s["sunset"]
         sunset_warning_time = sunset_time - timedelta(minutes=15)
 
-        print(
-            f"Now: {now}, Sunset Time: {sunset_time}, Sunset Warning Time: {sunset_warning_time}"
-        )  # Debug point 3
-
-        # If the current time is within the sunset warning period
         if sunset_warning_time <= now < sunset_time:
-            print("Sending sunset reminder.")  # Debug point 5
             await send_sunset_reminder()
             current_date += timedelta(days=1)
-            await asyncio.sleep(
-                (sunset_time - now).total_seconds()
-            )  # Sleep until sunset
+            await asyncio.sleep((sunset_time - now).total_seconds())
 
-        # If the current time is past sunset
         elif now >= sunset_time:
             current_date += timedelta(days=1)
 
-        # If neither of the above
         else:
             time_until_warning = (sunset_warning_time - now).total_seconds()
-            await asyncio.sleep(time_until_warning)  # Sleep until sunset warning time
+            await asyncio.sleep(time_until_warning)
 
         last_day_of_month = calendar.monthrange(today.year, today.month)[1]
         reminder_start_day = last_day_of_month - 4
+
         if (
             reminder_start_day <= today.day <= last_day_of_month
             and not db_handler.is_rent_paid()
         ):
             await send_rent_reminder()
 
-        await asyncio.sleep(1)  # Prevent 100% CPU usage
+        await asyncio.sleep(1)
 
 
 async def send_rent_reminder():
@@ -192,30 +182,14 @@ async def send_rent_reminder():
 
 async def send_sunset_reminder():
     opted_in_users = db_handler.get_users()
-    user_mentions = []
-    for row in opted_in_users:
-        user = bot.get_user(row[0])
-        if user:
-            user_mentions.append(user.mention)
-            logger.info("Added user %s to sunset reminder", row[0])
-
-    today = datetime.now(pytz.timezone(location_timezone)).date()
-    last_day_of_month = calendar.monthrange(today.year, today.month)[1]
-    fifth_day_before_end = (
-        today == datetime(today.year, today.month, last_day_of_month - 4).date()
-    )
+    user_mentions = [
+        bot.get_user(row[0]).mention for row in opted_in_users if bot.get_user(row[0])
+    ]
 
     sunset_message = f"Just a reminder that the sun will set in fifteen minutes! {', '.join(user_mentions)}"
-
     sunset_channel = bot.get_channel(bot.announce_channel_id)
     await sunset_channel.send(sunset_message)
     logger.info("Sent sunset reminder to channel %s", bot.announce_channel_id)
-
-    if fifth_day_before_end and not db_handler.is_rent_paid():
-        rent_message = "A friendly reminder: Rent is due in five days!"
-        rent_channel = bot.get_channel(bot.rent_reminder_channel_id)
-        await rent_channel.send(rent_message)
-        logger.info("Sent rent reminder to channel %s", bot.rent_reminder_channel_id)
 
 
 logger.info("Running bot!")
